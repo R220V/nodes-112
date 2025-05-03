1a6300174e2224b9365fbc2d3442c477418fd8f7
Krus <krus.margo@gmail.com>
2025-05-03 23:52:56 +0300

    mjs

--- lesson-2/01-path/index.js
+++ lesson-2/01-path/index.js
@@ -1,5 +1,15 @@
+// ESM
 import { readMovies } from './movies/movies.js';
 
 readMovies()
   .then((movies) => console.log(movies))
   .catch((err) => console.error(err));
+
+// ---------------------------------------------
+// COMMON JS
+
+// const {readMovies} = require("./movies");
+
+// readMovies()
+// .then(movies => console.log(movies))
+// .catch(err => console.error(err));
--- lesson-2/01-path/movies/movies.js
+++ lesson-2/01-path/movies/movies.js
@@ -1,11 +1,23 @@
-import * as fs from 'node:fs/promises'; // const fs = require("node:fs/promises");
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import * as fs from "node:fs/promises"; 
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 export function readMovies() {
-  const filename = fileURLToPath(import.meta.url);
-  const dirname = path.dirname(filename);
-  const filePath = path.join(dirname, 'movies.txt'); // path.join(import.meta.dirname, "movies.txt"); // v20.11.0
+  return fs.readFile('movies.txt', { encoding: 'utf-8'});
+  }
+//   const filename = fileURLToPath(import.meta.url);
+//   const dirname = path.dirname(filename);
+//   const filePath = path.join(dirname, 'movies.txt'); // path.join(import.meta.dirname, "movies.txt"); // v20.11.0
 
-  return fs.readFile(filePath, { encoding: 'utf-8' });
-}
+  // return fs.readFile(filePath, { encoding: 'utf-8' });
+// }
+// -----------------------------------------------------------------------------
+// const fs = require('node:fs/promises');
+
+// function readMovies(){
+//   return fs.readFile('movies.txt', { encoding: 'utf-8'});
+// }
+// //export for index
+// module.exports = {
+//   readMovies
+// };
